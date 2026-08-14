1c109387338bed65d1dc18a4392879bb4dde5448
Amit Dey <141947522+amitdey20@users.noreply.github.com>
2026-08-15 02:22:40 +0600

    Update OctalToHexadecimal.java

--- OctalToHexadecimal.java
+++ OctalToHexadecimal.java
@@ -1,11 +1,15 @@
-import java.util.Scanner;  
-public class OctalToHexadecimal { 
-    public static void main(String[] args) { 
-        Scanner in = new Scanner(System.in); 
-        System.out.print("Input a octal number : "); 
-        String oct = in.next();         
-        int dec = Integer.parseInt(oct, 8);         
-System.out.println("Equivalent hexadecimal number: " + Integer.toHexString(dec).toUpperCase()); 
-        in.close(); 
-    } 
+import java.util.Scanner;
+
+public class OctalToHexadecimal {
+public static void main(String[] args) {
+Scanner input=new Scanner(System.in) ;
+
+System.out.println("Input an Octal number:");
+String octal=input.next();
+int decimal = Integer.parseInt(octal, 8); 
+
+System.out.println("Hexadecimal number is:" +Integer.toHexString(decimal).toUpperCase());
+
+input.close();
+}    
 }
